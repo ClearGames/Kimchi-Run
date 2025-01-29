4b6e606021696d2735f0a1c1953421fbe09381ee
sVyu <vyuvyuvyu@naver.com>
2025-01-29 15:33:01 +0900

    feat: 스크롤링 스크립트 (Scrolling Script)

---
+++ Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour
+{
+    [Header("Settings")]
+    [Tooltip("How fast should the texture scroll?")]
+    [SerializeField] private float scrollSpeed;
+
+    [Header("References")]
+    [SerializeField] private MeshRenderer meshRenderer;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        meshRenderer.material.mainTextureOffset += new Vector2(scrollSpeed*Time.deltaTime, 0);
+    }
+}
