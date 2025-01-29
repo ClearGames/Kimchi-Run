d852a09e6515ac4263a34a81b7a3e975279d0f86
sVyu <vyuvyuvyu@naver.com>
2025-01-29 18:15:43 +0900

    feat: Player Hit & Heal

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -7,9 +8,12 @@ public class Player : MonoBehaviour
 
     [Header("References")]
     [SerializeField] private Rigidbody2D playerRigidBody;
-    [SerializeField] private Animator PlayerAnimator;
+    [SerializeField] private Animator playerAnimator;
+    [SerializeField] private BoxCollider2D playerBoxCollider;
 
     private bool isGrounded = true;
+    private int lives = 3;
+    private bool isInvincable = false;
 
     private void Update()
     {
@@ -18,7 +22,7 @@ public class Player : MonoBehaviour
             //playerRigidBody.linearVelocityY = 10;
             playerRigidBody.AddForceY(jumpForce, ForceMode2D.Impulse);
             isGrounded = false;
-            PlayerAnimator.SetInteger("state", 1);
+            playerAnimator.SetInteger("state", 1);
         }
     }
 
@@ -28,25 +32,58 @@ public class Player : MonoBehaviour
         {
             if (!isGrounded)
             {
-                PlayerAnimator.SetInteger("state", 2);
+                playerAnimator.SetInteger("state", 2);
             }
             isGrounded = true;
         }
     }
 
-    //private void OnTriggerEnter2D(Collider2D collision)
-    //{
-    //    if(collision.gameObject.tag == "enemy")
-    //    {
+    void KillPlayer()
+    {
+        playerBoxCollider.enabled = false;
+        playerAnimator.enabled = false;
+        playerRigidBody.AddForceY(jumpForce, ForceMode2D.Impulse);
+    }
 
-    //    }
-    //    else if(collision.gameObject.tag == "food")
-    //    {
+    private void Hit()
+    {
+        lives -= 1;
+        if (lives == 0) KillPlayer();
+    }
+    private void Heal()
+    {
+        lives = Mathf.Min(3, lives + 1);
+    }
 
-    //    }
-    //    else if (collision.gameObject.tag == "golden")
-    //    {
+    private void StartInvincible()
+    {
+        isInvincable = true;
+        Invoke("StopInvincible", 5f);
+    }
+    private void StopInvincible()
+    {
+        isInvincable = false;
+    }
 
-    //    }
-    //}
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "enemy")
+        {
+            if(!isInvincable) Destroy(collision.gameObject);
+            Hit();
+        }
+        else if (collision.gameObject.tag == "food")
+        {
+            Destroy(collision.gameObject);
+            Heal();
+        }
+        else if (collision.gameObject.tag == "golden")
+        {
+            Destroy(collision.gameObject);
+            StartInvincible();
+        }
+    }
+
 }
